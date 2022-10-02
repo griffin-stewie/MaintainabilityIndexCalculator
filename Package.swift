302c16a282ca59821f731a3cc94553dcc5be2650
griffin-stewie <panterathefamilyguy@gmail.com>
2022-10-02 23:24:26 +0900

    fix binary version lib_InternalSwiftSyntaxParser issues
    see https://github.com/realm/SwiftLint/blob/main/Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MaintainabilityIndexCalculator",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", exact: "0.50600.1"),
+        .package(url: "https://github.com/apple/swift-syntax", branch: "0.50700.0"),
     ],
     targets: [
         .executableTarget(
@@ -14,13 +14,28 @@ let package = Package(
             dependencies: [
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxParser", package: "swift-syntax"),
-            ]),
+                "lib_InternalSwiftSyntaxParser",
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
+            ]
+        ),
         .testTarget(
             name: "MaintainabilityIndexCalculatorTests",
             dependencies: [
                 "MaintainabilityIndexCalculator",
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxParser", package: "swift-syntax"),
-            ]),
+                "lib_InternalSwiftSyntaxParser",
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
+            ]
+        ),
+        .binaryTarget(
+            name: "lib_InternalSwiftSyntaxParser",
+            url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.7/lib_InternalSwiftSyntaxParser.xcframework.zip",
+            checksum: "99803975d10b2664fc37cc223a39b4e37fe3c79d3d6a2c44432007206d49db15"
+        ),
     ]
 )
